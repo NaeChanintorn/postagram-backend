c01071fc61dd61fc30d3302e6ccb7b928af42f46
NaeChanintorn <chanintorn.j39@gmail.com>
2024-02-25 00:04:12 +0700

    finished

--- src/middlewares/rate-limit.js
+++ src/middlewares/rate-limit.js
@@ -2,6 +2,6 @@ const { rateLimit } = require("express-rate-limit");
 
 module.exports = rateLimit({
   windowMs: 1000 * 60 * 5,
-  limit: 450,
+  limit: 300,
   message: { message: "Too many request please wait for a moment" },
 });
